4068b8135db82e2c5a03241bc531b52d6862993a
Joe bae <lineanity@gmail.com>
2024-12-09 10:15:03 +0700

    minor fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -27,39 +27,42 @@ export const authOptions = {
           type: "text",
           placeholder: "0x0",
         },
+        csrfToken: {
+          label: "CSRF Token",
+          type: "text",
+          placeholder: "Enter CSRF token",
+        },
       },
-      async authorize(credentials, req) {
-        const csrfToken = (req.body as { csrfToken: string }).csrfToken;
-      
+      async authorize(credentials) {
+        const csrfToken = credentials?.csrfToken;
+
         const appClient = createAppClient({
           ethereum: viemConnector(),
         });
-      
+
         const verifyResponse = await appClient.verifySignInMessage({
           message: credentials?.message as string,
           signature: credentials?.signature as `0x${string}`,
           domain: "scratchnism.vercel.app",
-          nonce: csrfToken,
+          nonce: csrfToken as string,
         });
-      
+
         const { success, fid } = verifyResponse;
-      
+
         if (!success) {
           return null;
         }
-      
+
         return {
           id: fid.toString(),
           name: credentials?.name,
           image: credentials?.pfp,
         };
       },
-      
     }),
   ],
 };
 
-// Default handler
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
